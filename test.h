0588b8bab10e169d5b4c903eee89ca6597b077bd
zouhirel <zouhairelmallah06@gmail.com>
2023-08-18 12:48:54 -0700

    test funcs

---
+++ test.h
@@ -0,0 +1,18 @@
+#ifndef _FUNCS_ARRAY_
+#define _FUNCS_ARRAY_
+
+/*
+*all the special characters for the printf function
+*/
+
+print_t funcs[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"d", print_int},
+	{"i", print_int},
+	{"u", print_unsigned},
+	{"o", print_octal},
+	{NULL, NULL}
+}
+
+#endif /* _FUNCS_ARRAY_ */
